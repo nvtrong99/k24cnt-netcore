4473b64eab5138b50686301c4ab5a2eea23acac6
nvtrong99 <vantrongbn99@gmail.com>
2026-08-27 10:51:21 +0700

    NvtLession02

---
+++ nvtDemoLesson02/nvtDemoLesson02/Controllers/NvtProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace nvtDemoLesson02.Controllers
+{
+	public class NvtProductController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
