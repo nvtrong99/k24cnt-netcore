fec3a0b3f361bf7c3b41fcc188f28279f569d6a8
nvtrong99 <vantrongbn99@gmail.com>
2026-09-06 20:06:14 +0700

    NvtLesson05Views - Fisrt WEBAPP

---
+++ NvtLesson05Views/NvtLesson05Views/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NvtLesson05Views.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
