c849b154df94aff01dde290a479c042ed56fcae3
nvtrong99 <vantrongbn99@gmail.com>
2026-09-12 22:07:20 +0700

    NvtLesson06 - Fisrt WEBAPP

---
+++ NvtLesson06/NvtLesson06/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace NvtLesson06.Models
+{
+	public class Category
+	{
+		public int CategoryId { get; set; }
+		public string CategoryName { get; set; }
+		public bool IsActive { get; set; }
+	}
+}
