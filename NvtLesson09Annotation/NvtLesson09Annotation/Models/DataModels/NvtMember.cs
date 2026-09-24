d1cb4b70a5f17eddab0251940fc4274795fc31df
nvtrong99 <vantrongbn99@gmail.com>
2026-09-24 08:45:33 +0700

    NvtLesson09Acc - Fisrt WEBAPP

---
+++ NvtLesson09Annotation/NvtLesson09Annotation/Models/DataModels/NvtMember.cs
@@ -0,0 +1,16 @@
+﻿namespace NvtLesson09Annotation.Models.DataModels
+{
+	
+	public class NvtMember
+	{
+		public int NvtMemberId { get; set; }
+		public string NvtUserName { get; set; }
+		public string NvtPassword { get; set; }
+		public string NvtEmail { get; set; }
+		public string NvtPhoneNumber { get; set; }
+		public string NvtFullName { get; set; }
+		public DateTime NvtBirthday { get; set; }
+
+
+	}
+}
---
+++ NvtLesson09Annotation/NvtLesson09Annotation/Models/DataViewModels/NvtMemberRegister.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace NvtLesson09Annotation.Models.DataViewModels
+{
+	public class NvtMemberRegister
+	{
+		
+			public int NvtMemberId { get; set; }
+			[DisplayName("Tên Đăng Nhập")]
+			[Required(ErrorMessage = "Tên đăng nhập không để trống")]
+			[StringLength(20, MinimumLength = 3, ErrorMessage = " Tên đăng đăng có phải từ 8 -20 kí tự bao gồm cả chữ và số ")]
+
+			public string NvtUserName { get; set; }
+			[DisplayName("Mật khẩu")]
+			[Required(ErrorMessage = "Mật khẩu không được để chống")]
+			[DataType(DataType.Password)]
+
+			public string NvtPassword { get; set; }
+			public string NvtEmail { get; set; }
+			public string NvtNumberphone { get; set; }
+			public string NvtFullName { get; set; }
+			public DateTime NvtBirthday { get; set; }
+
+
+	}
+}
